e6b7c1ca41de0fcf3b771e368472dd39ebf1b3b6
Toantoanhacker <canhtoan9262022@gmail.com>
2025-10-24 23:27:31 +0700

    make it always redirected to the login page if the user is not logged in

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import Prescription from "./pages/Prescription";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Navbar from "./components/NavBar";
+import PrivateRoute from "./components/PrivateRoute";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <div className="container mt-4">
         <Routes>
-          <Route path="/" element={<MedicalRecords />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/medicines" element={<Medicines />} />
-          <Route path="/prescriptions" element={<Prescription />} />
-          <Route path="/profile" element={<Profile />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<MedicalRecords />} />
+            <Route path="/medicines" element={<Medicines />} />
+            <Route path="/prescriptions" element={<Prescription />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
