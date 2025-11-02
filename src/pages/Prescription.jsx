0ea4a4b98ec9e5f7576e5d620d46321474fe8b7d
Dũng <ttuan1998ba@gmail.com>
2025-11-02 23:44:18 +0700

    hoa don thuoc

--- src/pages/Prescription.jsx
+++ src/pages/Prescription.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { PRESCRIPTION_ROUTE } from "../ApiRoute";
+import PrescriptionsList from "../components/PrescriptionsList";
 
 export default function Prescription() {
-  return <div>Prescription</div>;
+  const [prescriptions, setPrescriptions] = useState([])
+  useEffect(() => {
+    async function getPrescriptionsList() {
+      try {
+        const data = await fetch(PRESCRIPTION_ROUTE);
+        const res = await data.json();
+        setPrescriptions(res);
+
+      } catch (error) {
+        console.error("Lỗi hiển thị dữ liệu đơn thuốc", error);
+      }
+    }
+    getPrescriptionsList();
+
+  }, []);
+  return(
+  <div style={{
+        minHeight: "100vh", 
+        backgroundColor: "#f8f9fa",
+        paddingTop: "20px",
+      }} className="text-black">
+   <PrescriptionsList prescriptions={prescriptions}/>
+  </div>
+  );
 }
